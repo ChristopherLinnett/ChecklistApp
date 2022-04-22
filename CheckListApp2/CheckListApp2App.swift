628560b8f20dccf0979812a52911cd57133fa990
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-04-22 10:46:29 +1000

    Initial Commit

---
+++ CheckListApp2/CheckListApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  CheckListApp2App.swift
+//  CheckListApp2
+//
+//  Created by Christopher Linnett on 22/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct CheckListApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckListApp2UITests/CheckListApp2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CheckListApp2UITestsLaunchTests.swift
+//  CheckListApp2UITests
+//
+//  Created by Christopher Linnett on 22/4/2022.
+//
+
+import XCTest
+
+class CheckListApp2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
