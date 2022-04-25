de986932297f51a4cc0fe8a20965ce1eb0c95f17
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-04-25 14:18:57 +1000

    Add home view using other file as a template and modifying to fit structs

---
+++ CheckListApp2/views/HomeView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  CheckListApp2
+//
+//  Created by Christopher Linnett on 22/4/2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @ObservedObject var viewModel:ViewModel = ViewModel()
+    @State var newListTitle:String = ""
+    @Environment(\.editMode) var editMode
+    var body: some View {
+        NavigationView {
+            List{
+                ForEach (self.viewModel.mainList.file.indices, id: \.self) {index in
+                    NavigationLink(destination: ListDetailView(checklist: self.viewModel, selfIndex: index)) {Text(self.viewModel.mainList.file[index].checkListName)}
+            }.onDelete(perform:viewModel.deleteChecklist)
+            }
+            .padding(.leading)
+            .navigationTitle("Checklists")
+                .toolbar {
+                    ToolbarItemGroup(placement:.navigationBarLeading){
+                        EditButton()
+                    }
+                    ToolbarItemGroup(placement:.navigationBarTrailing) {
+                        newChecklistButton(viewModel: viewModel)
+                    }
+                }
+        }
+        
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(viewModel:ViewModel())
+    }
+}
