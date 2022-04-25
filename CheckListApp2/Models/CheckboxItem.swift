7549573fd0462ccc0a4def5344751898186d10a2
ChristopherLinnett <chrislinnett45389@gmail.com>
2022-04-25 14:17:35 +1000

    Add models

---
+++ CheckListApp2/Models/CheckboxItem.swift
@@ -0,0 +1,23 @@
+//
+//  CheckboxItem.swift
+//  CheckListApp2
+//
+//  Created by Christopher Linnett on 22/4/2022.
+//
+
+import Foundation
+
+struct CheckBoxListItem : Identifiable {
+    var id = UUID()
+    var itemName : String
+    var itemChecked : Bool = false
+    
+    mutating func toggleChecked() {              //allows individual checkbox to be checked/unchecked when clicked
+        self.itemChecked.toggle()
+    }
+    mutating func setFalse() {                   //allows resetting of checkmarks for reset function
+        self.itemChecked = false
+    }
+
+}
+
---
+++ CheckListApp2/Models/MasterListModel.swift
@@ -0,0 +1,20 @@
+//
+//  MasterListModel.swift
+//  Checklist
+//
+//  Created by Christopher Linnett on 31/3/2022.
+//
+
+import Foundation
+
+struct MasterListObject: Identifiable {
+    var id = UUID()
+    var checkListName:String
+    var checkListContainer: [CheckBoxListItem]
+    
+    mutating func deleteListItem(atOffsets: IndexSet){
+        self.checkListContainer.remove(atOffsets: atOffsets)
+    }
+ 
+
+}
